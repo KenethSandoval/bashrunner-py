2b99186174e77641e5e2220ad7e25ab9859a01a2
KenethSandoval <kenethra.74@gmail.com>
2020-11-18 15:29:27 -0600

    [feat]: Processing commands 2

--- bashrunner.py
+++ bashrunner.py
@@ -20,6 +20,12 @@ class BashRunner:
         
         elif command.create != None:
            self.__createContainer(command.create) 
+        elif command.set != None:
+            self.__setCurrentContainer(command.set)
+        elif command.list:
+            self.__getListOfContainer()
+        elif command.delete != None:
+            self.__deleteContainer(command.delete)
 
     # Imprime el nombre del contenedor actual
     def __printCurrentContainer(self):
@@ -27,7 +33,7 @@ class BashRunner:
         if currentContainer == None:
             print("There is currently no container configured")
         else:
-            print("Current container %s" % currentContainer)
+            print("The Current container is %s" % currentContainer)
 
     # Crea un contenedor vacio
     def __createContainer(self, name):
@@ -38,6 +44,49 @@ class BashRunner:
             self.containerManager.containers.append(newContainer)
             self.containerManager.writeSettingsFile()
             print("Container has been created %s" % name)
+
+    # Establece el contendor actual
+    def __setCurrentContainer(self, name):
+        index = self.containerManager.getContainerIndex(name)
+        if index == None:
+            print("Cannot configure container %s" % index)
+        else :
+            self.containerManager.currentContainer = name
+            self.containerManager.writeSettingsFile()
+            print("The current container is %s" % name)
+    
+    # Imprime la lista de contenedores disponibles
+    def __getListOfContainer(self):
+        containers = self.containerManager.containers
+        if len(containers) == 0:
+            print("No containers")
+        else:
+            for container in containers:
+                print("- %s" % container.name)
+    
+    # Elimina un contenedor
+    def __deleteContainer(self, containers):
+        if len(containers) == 0:
+            if self.containerManager.currentContainer == None:
+                containers = []
+            else:
+                containers = [self.containerManager.currentContainer]
+
+        while len(containers) != 0:
+            name = containers.pop(0)
+            index = self.containerManager.getContainerIndex(name)
+            
+            if index == None:
+                print("Container does not exist %s" % name)
+            else:
+                self.containerManager.containers.pop(index)
+                print("Removing... %s" % name)
+
+            if self.containerManager.currentContainer == name:
+                self.containerManager.currentContainer = None
+                print("Removing with container default %s" % name)
+        self.containerManager.writeSettingsFile()
+        print("Successfully removed")
 
 def parseArguments():
     parser = ArgumentParser()
